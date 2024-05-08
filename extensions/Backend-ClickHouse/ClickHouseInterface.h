4a80608d633b418bfd41355f028e694dc6fc59d4
g7ea5t1 <boris.momot@dxc.com>
2024-05-08 20:41:53 +0300

    feat: add clickhouse logdb

--- extensions/Backend-ClickHouse/ClickHouseInterface.h
+++ extensions/Backend-ClickHouse/ClickHouseInterface.h
@@ -17,74 +17,63 @@
 #ifndef ClickHouseInterface_H_
 #define ClickHouseInterface_H_
 // ---------------------------------------------------------------------------
+#include <DBInterface.h>
+#include <clickhouse/block.h>
+#include <clickhouse/client.h>
+#include <memory>
 #include <string>
 #include <vector>
-#include <memory>
-#include <clickhouse-cpp/client.h>
-#include <clickhouse-cpp/block.h>
-#include <DBInterface.h>
 // -------------------------------------------------------------------------
-namespace uniset
-{
-    // ----------------------------------------------------------------------------
-    class ClickHouseInterface:
-        public DBNetInterface
-    {
-        public:
+namespace uniset {
+// ----------------------------------------------------------------------------
+class ClickHouseInterface : public DBNetInterface {
+public:
+  ClickHouseInterface();
+  ~ClickHouseInterface();
 
-            ClickHouseInterface();
-            ~ClickHouseInterface();
+  virtual bool nconnect(const std::string &host, const std::string &user,
+                        const std::string &pswd, const std::string &dbname,
+                        unsigned int port = 9000) override;
+  virtual bool close() override;
+  virtual bool isConnection() const override;
+  virtual bool ping() const override;
 
-            virtual bool nconnect( const std::string& host, const std::string& user,
-                                   const std::string& pswd, const std::string& dbname,
-                                   unsigned int port = 9000) override;
-            virtual bool close() override;
-            virtual bool isConnection() const override;
-            virtual bool ping() const override;
+  virtual const std::string lastQuery() override;
 
-            virtual const std::string lastQuery() override;
+  // Unsupport types: Array,Nullable,Tuple, idx
+  virtual DBResult query(const std::string &q) override;
 
-            // Unsupport types: Array,Nullable,Tuple, idx
-            virtual DBResult query( const std::string& q ) override;
+  // supported all types
+  const std::vector<clickhouse::Block> bquery(const std::string &q);
 
-            // supported all types
-            const std::vector<clickhouse::Block> bquery( const std::string& q );
+  bool execute(const std::string &q);
+  bool insert(const std::string &tblname, const clickhouse::Block &data);
 
-            bool execute( const std::string& q );
-            bool insert( const std::string& tblname, const clickhouse::Block& data );
+  virtual const std::string error() override;
 
-            virtual const std::string error() override;
+  bool reconnect(const std::string &host, const std::string &user,
+                 const std::string &pswd, const std::string &dbname,
+                 unsigned int port = 9000);
 
-            bool reconnect(const std::string& host, const std::string& user,
-                           const std::string& pswd, const std::string& dbname,
-                           unsigned int port = 9000);
+  void setOptions(int sendRetries, bool pingBeforeQuery);
 
-            void setOptions( int sendRetries, bool pingBeforeQuery );
+  // unsupported
+  virtual bool insert(const std::string &q) override { return false; }
+  virtual double insert_id() override { return -1; }
 
-            // unsupported
-            virtual bool insert( const std::string& q ) override
-            {
-                return false;
-            }
-            virtual double insert_id() override
-            {
-                return -1;
-            }
+protected:
+private:
+  DBResult makeResult(const clickhouse::Block &res);
+  void appendResult(DBResult &ret, const clickhouse::Block &block);
 
-        protected:
+  std::unique_ptr<clickhouse::Client> db;
+  std::string lastQ;
+  std::string lastE;
 
-        private:
-            DBResult makeResult( const clickhouse::Block& res );
-            void appendResult( DBResult& ret, const clickhouse::Block& block );
-
-            std::unique_ptr<clickhouse::Client> db;
-            std::string lastQ;
-            std::string lastE;
-
-            int sendRetries = { 2 };
-            bool pingBeforeQuery = { true };
-    };
-    // ----------------------------------------------------------------------------------
+  int sendRetries = {2};
+  bool pingBeforeQuery = {true};
+};
+// ----------------------------------------------------------------------------------
 } // end of namespace uniset
 // ----------------------------------------------------------------------------
 #endif
